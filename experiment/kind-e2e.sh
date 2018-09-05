f0941a7a3b4d82e5251e7ec7a5e5610299b5ecfe
Benjamin Elder <bentheelder@google.com>
2018-09-04 18:49:47 -0700

    .kube

--- experiment/kind-e2e.sh
+++ experiment/kind-e2e.sh
@@ -78,7 +78,7 @@ fi
 
 # export the KUBECONFIG
 # TODO(bentheelder): provide a `kind` command that can be eval'ed instead
-export KUBECONFIG="${HOME}/.config/kind-config-1"
+export KUBECONFIG="${HOME}/.kube/kind-config-1"
 
 # setting this env prevents ginkg e2e from trying to run provider setup
 export KUBERNETES_CONFORMANCE_TEST="y"
